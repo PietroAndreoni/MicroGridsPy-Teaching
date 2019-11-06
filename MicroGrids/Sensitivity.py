2563e73813f32dd17be5a73f14fd9bf8096b8990
Pietro Andreoni <pietro.andreoni@outlook.com>
2019-11-06 12:59:11 +0100

    Added function for .dat modifications
    Never run, to be tested
    Also added i/o extensions to gitignore file

--- MicroGrids/Sensitivity.py
+++ MicroGrids/Sensitivity.py
@@ -1,7 +1,17 @@
-sens_par_1 = [1,2,3]  # list of ordered values of parameter 1 you want to change
-sens_par_2 = [1]      # list of orderer values of parameter 2 you want to change
-line_i = 0            # line in which paramater 1 is written in data_MY.dat
-line_j = 0            # line in which paramater 2 is written in data_MY.dat
+sens_par_i = [1,2,3]  # list of ordered values of parameter i you want to change
+sens_par_j = [1]      
+
+par_i = 0             # name of parameter i
+par_j = 0             
+
+line_i = 0            # line in which paramater i is written in data_MY.dat (for multiD parameters it refers to the declaration line "param:  ecc")
+line_j = 0           
+
+ind_i = 1             # index of the parameter i for multimensional parameter
+ind_j = 1
+
+dim_i = 1             # dimension for the parameter i for multidimensional parameters
+dim_j = 1
 
 for i in range(len(sens_par_1)):
     for j in range(len(sens_par_2)):
@@ -26,8 +36,8 @@ for i in range(len(sens_par_1)):
 
         model = AbstractModel() # define type of optimization problem
 
-        Modify_input(sens_par_1(i),line_i)
-        Modify_input(sens_par_2(j),line_j)
+        Modify_input(par_i,sens_par_i(i),line_i,ind_i,dim_i)
+        Modify_input(par_j,sens_par_j(j),line_j,ind_i,dim_i)
 
         # Optimization model    
         Model_Creation(model, Renewable_Penetration, Battery_Independency) # Creation of the Sets, parameters and variables.
@@ -36,6 +46,20 @@ for i in range(len(sens_par_1)):
         Data[i][j] = Load_Results(instance, Optimization_Goal)
 
 
-def Modify_input(value,line):
+def Modify_input(variable,value,line,ind,dim):
 
-    #write inputs into 
+    f = open('Inputs/data_MY.dat','r')
+    lines = f.readlines()
+
+    if dim==1
+        lines[line-1] = "param: " + str(variable) + " := " + str(value) + ";"
+    elif ind!=dim
+        lines[line-1+dim] = str(ind) + "  " +  str(value)
+    else
+        lines[line-1+dim] = str(ind) + "  " +  str(value) + ";"
+
+    f.close()
+
+    f = open('Inputs/data_MY.dat','w')
+    f.writelines(lines)
+    f.close()
