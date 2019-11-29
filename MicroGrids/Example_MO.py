2203a25f6a5b8ddcf9c548127a00eb367e9f71c1
Pietro Andreoni <pietro.andreoni@outlook.com>
2019-11-29 16:37:24 +0100

    Added lexicographic method for payoff matrix

--- MicroGrids/Example_MO.py
+++ MicroGrids/Example_MO.py
@@ -32,59 +32,57 @@ model.C_f2 = Constraint(expr= model.f2 == 3 * model.X1 + 4 * model.X2)
 model.O_f1 = Objective(expr= model.f1  , sense=maximize)
 model.O_f2 = Objective(expr= model.f2  , sense=maximize)
 
+# ## max f1
 model.O_f2.deactivate()
 
 solver = SolverFactory('gurobi')
 solver.solve(model);
 
-print( '( X1 , X2 ) = ( ' + str(value(model.X1)) + ' , ' + str(value(model.X2)) + ' )')
-print( 'f1 = ' + str(value(model.f1)) )
-print( 'f2 = ' + str(value(model.f2)) )
-f2_min = value(model.f2)
-
+f1_max = value(model.f1)
 
 # ## max f2
 
 model.O_f2.activate()
 model.O_f1.deactivate()
 
-solver = SolverFactory('gurobi')
 solver.solve(model);
 
-print( '( X1 , X2 ) = ( ' + str(value(model.X1)) + ' , ' + str(value(model.X2)) + ' )')
-print( 'f1 = ' + str(value(model.f1)) )
-print( 'f2 = ' + str(value(model.f2)) )
 f2_max = value(model.f2)
+
+# ## min f1
+model.O_f2.deactivate()
+model.O_f1.activate()
+
+model.f2.fix(f2_max)
+
+f1_min = value(model.f1)
+
+
+# ## min f2 
+model.O_f2.activate()
+model.O_f1.deactivate()
+
+model.f2.unfix()
+model.f1.fix(f1_max)
+
+solver.solve(model)
+
+f2_min = value(model.f2)
+
+print('Each iteration will keep f2 lower than some values between f2_min and f2_max, so ['       + str(f2_min) + ', ' + str(f2_max) + ']')
 
 
 # ## apply normal $\epsilon$-Constraint
 
 model.O_f1.activate()
 model.O_f2.deactivate()
+model.f1.unfix()
 
 model.e = Param(initialize=0, mutable=True)
 
-model.C_epsilon = Constraint(expr = model.f2 == model.e)
-
-solver.solve(model);
-
-print('Each iteration will keep f2 lower than some values between f2_min and f2_max, so ['       + str(f2_min) + ', ' + str(f2_max) + ']')
-
 n = 4
 step = int((f2_max - f2_min) / n)
-steps = list(range(int(f2_min),int(f2_max),step)) + [f2_max]
-
-x1_l = []
-x2_l = []
-for i in steps:
-    model.e = i
-    solver.solve(model);
-    x1_l.append(value(model.X1))
-    x2_l.append(value(model.X2))
-plt.plot(x1_l,x2_l,'o-.');
-plt.title('inefficient Pareto-front');
-plt.grid(True);
-
+steps = list(range(int(f2_min),int(f2_max+step),step)) 
 
 # ## apply augmented $\epsilon$-Constraint
 
@@ -93,7 +91,6 @@ plt.grid(True);
 
 model.del_component(model.O_f1)
 model.del_component(model.O_f2)
-model.del_component(model.C_epsilon)
 
 model.delta = Param(initialize=0.00001)
 
@@ -103,9 +100,10 @@ model.O_f1 = Objective(expr = model.f1 + model.delta * model.s, sense=maximize)
 
 model.C_e = Constraint(expr = model.f2 - model.s == model.e)
 
+
 x1_l = []
 x2_l = []
-for i in range(160,190,6):
+for i in steps:
     model.e = i
     solver.solve(model);
     x1_l.append(value(model.X1))
